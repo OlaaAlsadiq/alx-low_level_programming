fae7f3c367773c624a12489caa1eca9ec3b659a5
OlaaAlsadiq <olaasumel7@gmail.com>
2023-10-19 16:56:45 -0700

    fml

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,25 +1,17 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <stddef.h>
-
-/* Struct for a singly-linked list node */
-typedef struct list_s {
-    char *str;
-    struct list_s *next;
-} list_t;
-
-/* Function prototype for adding a node at the end of the list */
-list_t *add_node_end(list_t **head, const char *str);
 
 /**
- * struct list_s - Singly linked list
- * @str: String (malloc'ed string)
- * @len: Length of the string
- * @next: Pointer to the next node
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
@@ -28,7 +20,11 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
-/* Function to print elements of a linked list */
+int _putchar(char c);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
